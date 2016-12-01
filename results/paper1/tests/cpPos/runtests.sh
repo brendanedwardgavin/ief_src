0449ca8f8d726f49048b42045e818c0c91974068
Brendan Gavin <brendanedwardgavin@gmail.com>
2016-12-01 11:19:23 -0500

    test file updates

--- results/paper1/tests/cpPos/runtests.sh
+++ results/paper1/tests/cpPos/runtests.sh
@@ -3,7 +3,11 @@ bindir=../../../../bin
 matdir=../../matrices
 
 echo "Na5 ellipse"
-$bindir/time_sddriver $matdir/Na5 Na5_ellipse/Na5 >Na5_ellipse/Na5.out
+#$bindir/time_sddriver $matdir/Na5 Na5_ellipse/Na5 >Na5_ellipse/Na5.out
 echo "Na5 line"
-$bindir/time_sddriver $matdir/Na5 Na5_line/Na5 >Na5_line/Na5.out
+#$bindir/time_sddriver $matdir/Na5 Na5_line/Na5 >Na5_line/Na5.out
+echo "Na5 ellipse mid"
+$bindir/time_sddriver $matdir/Na5 Na5_ellipse_mid/Na5 >Na5_ellipse_mid/Na5.out
+echo "Na5 line mid"
+$bindir/time_sddriver $matdir/Na5 Na5_line_mid/Na5 >Na5_line_mid/Na5.out
 
--- results/paper1/tests/practical/runtests.sh
+++ results/paper1/tests/practical/runtests.sh
@@ -3,19 +3,19 @@ bindir=../../../../bin
 matdir=../../matrices
 
 echo "CO gmres"
-$bindir/time_sddriver $matdir/CO CO_gm/CO > CO_gm/CO.out
+#$bindir/time_sddriver $matdir/CO CO_gm/CO > CO_gm/CO.out
 echo "CO pardiso"
-$bindir/time_sddriver $matdir/CO CO_pa/CO > CO_pa/CO.out
+#$bindir/time_sddriver $matdir/CO CO_pa/CO > CO_pa/CO.out
 echo "Ga3As3H12 gmres"
 $bindir/time_sddriver $matdir/Ga3As3H12 Ga3As3H12_gm/Ga3As3H12 > Ga3As3H12_gm/Ga3As3H12.out
 echo "Ga3As3H12 pardiso"
 $bindir/time_sddriver $matdir/Ga3As3H12 Ga3As3H12_pa/Ga3As3H12 > Ga3As3H12_pa/Ga3As3H12.out
 echo "Na5 gmres"
-$bindir/time_sddriver $matdir/Na5 Na5_gm/Na5 > Na5_gm/Na5.out
+#$bindir/time_sddriver $matdir/Na5 Na5_gm/Na5 > Na5_gm/Na5.out
 echo "Na5 Pardiso"
-$bindir/time_sddriver $matdir/Na5 Na5_pa/Na5 > Na5_pa/Na5.out
+#$bindir/time_sddriver $matdir/Na5 Na5_pa/Na5 > Na5_pa/Na5.out
 echo "Qe4213 gmres"
-$bindir/time_sddriver $matdir/H_matrix_4213 qe4213_gm/H_matrix_4213 > qe4213_gm/H_matrix_4213.out
+#$bindir/time_sddriver $matdir/H_matrix_4213 qe4213_gm/H_matrix_4213 > qe4213_gm/H_matrix_4213.out
 #echo "Qe4213 pardiso"
 #$bindir/time_sddriver $matdir/H_matrix_4213 qe4213_pa/H_matrix_4213 > qe4213_pa/H_matrix_4213.out
 
